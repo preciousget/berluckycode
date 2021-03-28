3585bc293713ff9edc7363c2286e0ec00e34b25d
preciousget <43610049+preciousget@users.noreply.github.com>
2021-03-28 17:07:42 +0700

    new commit

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var path = require('path');
 var fs = require('fs');
 
 
-//หฟหกฟหก
 https.get('https://spreadsheets.google.com/feeds/cells/1uGr0mIWn9pUflZQRuei1FepXv30JdQSBVWLUjL_Zpyw/1/public/full?alt=json', function(resp) {
     resp.setEncoding('utf8');
     var body = '';
@@ -19,22 +18,17 @@ https.get('https://spreadsheets.google.com/feeds/cells/1uGr0mIWn9pUflZQRuei1FepX
         var rawdata = JSON.parse(body);
         var entries = rawdata.feed.entry;
         for (var i = 9; i < entries.length; i = i + 9) {
-            var number_obj = ['number'];
-            var price_obj = ['price'];
-            var network_obj = ['network'];
-            var servicetype_obj = ['service'];
-            var tot = [];
-            var number = entries[i].content.$t;
-            var network = entries[i+1].content.$t;
-            var servicetype = entries[i+2].content.$t;
-            var price = entries[i+3].content.$t;
-            number_obj.push(number);price_obj.push(price);servicetype_obj.push(servicetype);network_obj.push(network);
-            tot.push(number_obj);tot.push(price_obj);tot.push(servicetype_obj);tot.push(network_obj);
-            json.push(Object.fromEntries(tot));
+            var tot = {};
+            tot.number = entries[i].content.$t;
+            tot.network = entries[i+1].content.$t;
+            tot.servicetype = entries[i+2].content.$t;
+            tot.price = entries[i+3].content.$t;
+            json.push(tot);
         };
 
-        var stockdata = Object.fromEntries([['total',entries.length/9-1],['details',json]]);
-        console.log(JSON.parse(JSON.stringify(stockdata)).details[1]);
+        var stockdata = {};
+        stockdata.total = entries.length/9-1;
+        stockdata.details = json;
 
         var result = json;
         result = sort_by_key(result ,'number');
